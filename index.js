e25b69af9c4de1b56aa517d5bb936e14181ec22c
Kyle Galer <chembot81@hotmail.com>
2020-12-02 16:49:39 -0600

    table of contents

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ inquirer
         {
             type: "input",
             name: "title",
-            message: "Please input your title:"
+            message: "Input your title:"
         },
         {
             type: "input",
             name: "description",
-            message: "Please describe your project:"
+            message: "Describe your project:"
         },
         {
             type: "input",
@@ -24,7 +24,55 @@ inquirer
             name: "usage",
             message: "Include any usage information here:"
         },
-        // license info to go here
-
-
+        {
+            type: "list", 
+            name: "license",
+            message: "Select a license for your project:",
+            choices: ["GNU", "Apache", "Berkeley Software Distribution", "MIT"],
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "Are there any contribution guidelines?"
+        },
+        {
+            type: "input",
+            name: "test",
+            message: "Input test instructions:"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Input your github username:"
+        },
+        {
+            type: "input",
+            name: "e-mail",
+            message: "Input your e-mail address:"
+        },
     ])
+    .then(response => {
+        console.log(response);
+        createReadMe(response);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    function createReadMe(response) {
+        let readMeContent = "";
+        // creating the title
+        readMeContent += `# ${response.title}\n`;
+        // creating the table of contents
+        readMeContent += `\n
+        Table of Contents\n
+        [Description](#description)\n
+        [Installation Instructions](#installation-instructions)\n
+        [Usage Information](#usage-information)\n
+        [License](#license)\n
+        [Contribution Guidelines](#contribution-guidelines)\n
+        [Test Instructions](#test-instructions)\n
+        [Github Username](#github-username)\n
+        [E-mail Address](#e-mail-address)\n`;
+
+        fs.writeFile("readme.md", readMeContent, error => error ? console.log(error) : console.log("Great Success!"));
+    }
---
+++ readme.md
@@ -0,0 +1,18 @@
+# MonkeyTrainer
+Table of Contents
+
+        [Description](#description)
+
+        [Installation Instructions](#installation-instructions)
+
+        [Usage Information](#usage-information)
+
+        [License](#license)
+
+        [Contribution Guidelines](#contribution-guidelines)
+
+        [Test Instructions](#test-instructions)
+
+        [Github Username](#github-username)
+
+        [E-mail Address](#e-mail-address)
